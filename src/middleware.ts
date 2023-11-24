5ddb955fb8d9d926c6fd858c565ab0d556579948
Alastair Deng <adeng27@stanford.edu>
2023-11-24 17:44:16 -0600

    fixed auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,33 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+// import { authMiddleware } from "@clerk/nextjs/server";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
  
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
+// // This example protects all routes including api/trpc routes
+// // Please edit this to allow other routes to be public as needed.
 
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({ publicRoutes: ["/"] });
+// // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+// export default authMiddleware({ publicRoutes: ["/"] });
  
+// export const config = {
+//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+ 
+export default withClerkMiddleware((req: NextRequest) => {
+  return NextResponse.next();
+});
+ 
+// Stop Middleware running on static files
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    "/((?!static|.*\\..*|_next|favicon.ico).*)",
+    "/",
+  ],
+}
