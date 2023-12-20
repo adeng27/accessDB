89cf6d9de22bd9341ef121e1496c65a808d7cbbc
Alastair Deng <adeng27@stanford.edu>
2023-12-20 01:44:26 -0600

    rate limiter

--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -6,13 +6,27 @@ import openai, { getEmbedding } from "~/lib/openai";
 import { createTRPCRouter, publicProcedure, privateProcedure } from "~/server/api/trpc";
 import { resourcesIndex } from "~/server/pinecone";
 
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { TRPCError } from "@trpc/server";
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(3, "1 m"),
+    analytics: true,
+  });
+
 export const openaiRouter = createTRPCRouter({
   chatCompletion: privateProcedure.input(
     z.object({
         prompt: z.string(),
     })
   ).mutation(async ({ctx, input: { prompt } }) => {
-    console.log(prompt);
+    const userId = ctx.userId;
+
+    const { success } = await ratelimit.limit(userId);
+    if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
 
     const embedding = await getEmbedding(prompt);
     const vectorQueryResponse = await resourcesIndex.query({
