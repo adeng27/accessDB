3bb29fd3cd8ecd9f1a84ed2561d2381bf14d3fce
Alastair Deng <adeng27@stanford.edu>
2023-12-23 17:53:35 -0600

    resourceview mobile pt 2

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -23,10 +23,10 @@ export const ResourceCard = (props: {id: string | undefined, name: string | unde
 
     return (
         <div>
-            <a className="flex flex-col gap-4 max-w-sm p-6 border rounded-lg shadow bg-gray-800 border-gray-700 hover:bg-gray-700">
+            <a className="flex flex-col md:gap-4 gap-2 max-w-sm p-6 border rounded-lg shadow bg-gray-800 border-gray-700 hover:bg-gray-700">
                 <h5 className="md:text-2xl text-base font-bold tracking-tight text-white">{props.name ? props.name : "Something went wrong"}</h5>
-                <p className="font-normal md:text-base text-sm text-gray-400">{props.description ? props.description : "Something went wrong"}</p>
-                <p className="font-extrabold md:text-base text-sm text-gray-400">{props.reqs ? props.reqs : "Something went wrong"}</p>
+                <p className="font-normal md:text-base text-xs text-gray-400">{props.description ? props.description : "Something went wrong"}</p>
+                <p className="font-extrabold md:text-base text-xs text-gray-400">{props.reqs ? props.reqs : "Something went wrong"}</p>
                 <p className="md:text-xl text-base font-extrabold text-gray-400">{props.benefit ? props.benefit : "Something went wrong"}</p>
                 <div className="flex justify-center">
                     <button 
--- src/components/resources.tsx
+++ src/components/resources.tsx
@@ -17,7 +17,7 @@ export const ViewResources = (props: {resources: resourceType[]}) => {
     const [startResourceSm, setStartResourceSm] = useState(0);
 
     return (
-        <div className="flex justify-center gap-8">
+        <div className="flex justify-center md:gap-8 gap-4">
             <div className="flex items-center justify-center">
                 <button 
                     type="button" 
@@ -36,7 +36,7 @@ export const ViewResources = (props: {resources: resourceType[]}) => {
                     disabled={startResourceSm === 0}
                     className="inline-flex text-white bg-blue-700 enabled:focus:ring-4 enabled:focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center md:hidden items-center enabled:bg-blue-600 enabled:hover:bg-blue-700 enabled:focus:ring-blue-800 disabled:bg-slate-700"
                 >
-                    <svg className="w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="w-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path>
                     </svg>
                 </button>
@@ -103,7 +103,7 @@ export const ViewResources = (props: {resources: resourceType[]}) => {
                     disabled={startResourceSm >= resources.length - 2}
                     className="text-white bg-blue-700 enabled:focus:ring-4 enabled:focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center md:hidden inline-flex items-center enabled:bg-blue-600 enabled:hover:bg-blue-700 enabled:focus:ring-blue-800 disabled:bg-slate-700"
                 >
-                    <svg className="w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="w-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                     </svg>
                 </button>
